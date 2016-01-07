1fd68bbe8221a901239575bf4502b683479702ef
ForzenHeart <junchenghe@outlook.com>
2016-01-08 01:25:41 +0800

    finish the script

--- readDir.sh
+++ readDir.sh
@@ -1,12 +1,21 @@
 #!/bin/bash -
 #iterate through all the files in a directory
 
-for file in *
+#替换该文件下面的所有文件
+for file in *.cpp *.h
 do
 	if [ -f "$file" ]
 	then
-		replacefile=`echo "$file" | grep "FAT*.*.bk"`;
-		echo $replacefile;
-		#sed -f fixup.sed $replacefile > "$replacefile.bk";
+		echo "replace data in $file"
+		sed -f fixup.sed $file > "$file.bk"
+		#移除已经被替换的源文件
+		rm -v $file
 	fi
+done
+
+#将已经替换好的文件重命名为原来的名字
+for replacefile in *.bk
+do
+	cp -nv $replacefile ${replacefile%.bk} 
+	rm -v $replacefile
 done
